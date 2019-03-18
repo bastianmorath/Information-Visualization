6ada5a0f96d2d06988caf2764df62addd402077a
bastianmorath <bastian.morath@gmx.ch>
2019-03-18 13:01:43 +0800

    Some changes to ass3

--- IV Assignment 3/code/ass3.py
+++ IV Assignment 3/code/ass3.py
@@ -9,22 +9,9 @@ data = []
 content = open(filename, "r").read() 
 f = json.loads("[" + content.replace("}\n{", "},\n{") + "]")
 
-df_original = json_normalize(f)
+df_original = json_normalize(f)[['title', 'authors', 'inCitations', 'year', 'venue']]
 
 
-# Top 5 cited papers:
-
-df_original['number_in_citations'] = df_original.apply (lambda row: len(row['inCitations']), axis=1)
-print('Top 5 cited papers at ArXiv: ')
-print(df_original[df_original['venue']=='ArXiv'].sort_values(by='number_in_citations', ascending=False)[['title', 'number_in_citations']].head(10))
-
-# Years of ICSE papers:
-
-print('Years of ICSE papers: ')
-df_public_ICSE = df_original[df_original['venue'] == 'ICSE']
-print(df_public_ICSE['year'])
-
-'''
 column_names = list(df_original) + ['author']
 del column_names[0]
 df = pd.DataFrame(columns=column_names)
@@ -32,13 +19,27 @@ df = pd.DataFrame(columns=column_names)
 for index, _ in df_original.iterrows():
 	row = df_original.loc[index, :].copy(deep=True)
 	authors = row['authors']
-	del row['authors']
 	for dict in authors:
 		row['author'] = dict['name']
 		df = df.append(row)
 
 df.reset_index(inplace=True)
-'''
+df['inCitations'] = df['inCitations'].apply(lambda x: len(x))
+df.drop(['authors'], axis=1, inplace=True)
+
+
+# Top 5 cited papers:
+
+print('Top 5 cited papers at ArXiv: ')
+print(df_original[df_original['venue']=='ArXiv'].sort_values(by='inCitations', ascending=False)[['title', 'inCitations']].head(10))
+
+# Years of ICSE papers:
+
+print('Years of ICSE papers: ')
+df_public_ICSE = df_original[df_original['venue'] == 'ICSE']
+print(df_public_ICSE['year'])
+
+
 
 from dataclasses import dataclass
 
@@ -46,9 +47,9 @@ from dataclasses import dataclass
 class ContributorIndex():
 
 
-	years_active: float = 1.0
-	number_of_papers: float = 0.0
-	total_in_citations: float = 0.0
+	years_active: int = 1
+	number_of_papers: int = 0
+	total_in_citations: int = 0
 	name: str = ''
 
 	@property
@@ -58,33 +59,28 @@ class ContributorIndex():
 contrib_list = []
 
 
-def filter_row(row, name):
-	for a in row['authors']:
-		if (a['name'] == name):
-			return True
-	return False
+# df_copy = df_original.copy(deep=True)
+authors_looked_at = []
 
-df_copy = df_original.copy(deep=True)
 for index, _ in df_original.iterrows():
-	row = df_copy.loc[index, :]
-	if len(row['authors']) != 0:
-		name = row['authors'][0]['name']
-		name_df = df_copy[df_copy.apply(lambda x: True in [a['name'] == name for a in x['authors']], axis=1)]
-		indices = name_df.index.tolist()
-		for ind in indices:
-			author_list = df_copy.loc[ind]['authors']
-			if len(author_list) == 0: # No authors left
-				df_copy.drop(df_copy.index[ind]) # Drop row
-			else:
-				df_copy.at[ind, 'authors'] = [a for a in author_list if a['name'] != name]
-		contrib_index = ContributorIndex()
-		contrib_index.name = name
-		contrib_index.years_active =  name_df.sort_values(by='year', ascending=False).iloc[0]['year'] - name_df.sort_values(by='year', ascending=False).iloc[-1]['year'] + 1
-		contrib_index.total_in_citations = name_df['number_in_citations'].sum()
-		contrib_index.number_of_papers = len(name_df.index)
+	df_original.at[index, 'authors'] = [a['name'] for a in df_original.at[index, 'authors']]
 
-		contrib_list.append(contrib_index)
+	row = df_original.loc[index, :]
+	for name in row['authors']:
+		if not name in authors_looked_at:
+			name_df = df_original[df_original.apply(lambda x: name in x['authors'], axis=1)].copy()
+			contrib_index = ContributorIndex()
+			contrib_index.name = name
+			name_df.fillna({'year' : 0}, inplace=True)
 
-for ci in contrib_list:
+			contrib_index.years_active =  int(name_df.sort_values(by='year', ascending=False).iloc[0]['year'] - name_df.sort_values(by='year', ascending=False).iloc[-1]['year'] + 1)
+			contrib_index.total_in_citations = name_df['inCitations'].sum()
+			contrib_index.number_of_papers = len(name_df.index)
+
+			contrib_list.append(contrib_index)
+			authors_looked_at.append(name)
+
+# contrib_list = contrib_list.sort(key=lambda x: x.index)
+for ci in contrib_list[:10]:
+	#print(ci.name + str(round(ci.index, 2)))
 	print(ci)
-	print(ci.index)
